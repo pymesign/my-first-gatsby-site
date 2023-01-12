0ef0174d48c28fede906a0eef7f0a43f5c0b8d75
Pymesign <info@pymesign.com>
2023-01-11 23:10:30 -0300

    Finished Gatsby tutorial part 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 // Step 1: Import React
 import * as React from 'react'
+import Layout from './components/Layout'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <main>
-      <h1>Welcome to my Gatsby site!</h1>
+    <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
+    </Layout>
   )
 }
 
