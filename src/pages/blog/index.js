1cf217e6fe70ac3d209efeb270780a9ec15fe917
Pymesign <info@pymesign.com>
2023-02-12 14:04:50 -0300

    Finished Gatsby Tutorial Part 6

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,16 +1,19 @@
 import * as React from "react";
-import { graphql } from "gatsby";
-import Layout from "./components/Layout";
-import Seo from "./components/Seo";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Seo from "../components/Seo";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       {data.allMdx.nodes.map((node) => (
         <article key={node.id}>
-          <h2>{node.frontmatter.title}</h2>
-          <p>Posted: {node.frontmatter.date}</p>
-          <p>{node.excerpt}</p>
+          <h2>
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+            <p>Posted: {node.frontmatter.date}</p>
         </article>
       ))}
     </Layout>
@@ -24,6 +27,7 @@ export const query = graphql`
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
+          slug
         }
         id
         excerpt
