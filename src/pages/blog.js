5fbf237bb3998408da5f05d804e8c1d3f4ce649f
Pymesign <info@pymesign.com>
2023-02-12 10:31:10 -0300

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,34 +1,37 @@
-import * as React from 'react'
-import { graphql } from 'gatsby'
-import Layout from './components/Layout'
-import Seo from './components/Seo'
+import * as React from "react";
+import { graphql } from "gatsby";
+import Layout from "./components/Layout";
+import Seo from "./components/Seo";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-      {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
-        ))
-      }
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <p>{node.excerpt}</p>
+        </article>
+      ))}
     </Layout>
-  )
-}
+  );
+};
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: { frontmatter: { date: DESC } }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt
       }
     }
   }
-`
+`;
 
-export const Head = () => <Seo title="My Blog Posts" />
+export const Head = () => <Seo title="My Blog Posts" />;
 
-export default BlogPage
+export default BlogPage;
