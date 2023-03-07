556207ae7e08b0a748ff176d82611184222a4d81
Pymesign <info@pymesign.com>
2023-03-07 09:11:21 -0300

    Changing state object in topFiveList and save to localStorage

--- src/pages/components/TopFiveList.js
+++ src/pages/components/TopFiveList.js
@@ -1,29 +1,65 @@
 import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const TopFiveList = () => {
-  const [items, setItems] = useState([
-    { id: 1, content: "Item 1" },
-    { id: 2, content: "Item 2" },
-    { id: 3, content: "Item 3" },
-    { id: 4, content: "Item 4" },
-    { id: 5, content: "Item 5" },
-  ]);
+  const [list, setList] = useState({
+    id: uuidv4(),
+    title: "My Top Five List",
+    items: [
+      { id: uuidv4(), content: "Item 1" },
+      { id: uuidv4(), content: "Item 2" },
+      { id: uuidv4(), content: "Item 3" },
+      { id: uuidv4(), content: "Item 4" },
+      { id: uuidv4(), content: "Item 5" },
+    ],
+  });
 
   const handleChange = (e, id) => {
-    setItems(
-      items.map((item) => {
+    setList((prevList) => {
+      const newItems = prevList.items.map((item) => {
         if (item.id === id) {
-          item.content = e.target.value;
+          return { ...item, content: e.target.value };
         }
         return item;
-      })
-    );
+      });
+      return { ...prevList, items: newItems };
+    });
   };
 
-  const handleSubmit = (event) => {
-    //aqui el evento que guarda
-    event.preventDefault();
-    console.log("saving");
+  const handleTitleChange = (e) => {
+    setList((prevState) => {
+      return { ...prevState, title: e.target.value };
+    });
+  };
+
+  const handleSave = () => {
+    try {
+      const savedLists = JSON.parse(localStorage.getItem("topFiveLists")) || [];
+      const newList = {
+        id: list.id,
+        title: list.title,
+        items: list.items.map((item) => ({ content: item.content })),
+      };
+      savedLists.push(newList);
+      localStorage.setItem("topFiveLists", JSON.stringify(savedLists));
+      resetForm();
+      console.log("Data saved: ", newList);
+    } catch (error) {
+      console.error("Error saving data: ", error);
+    }
+  };
+  const resetForm = () => {
+    setList({
+      id: uuidv4(),
+      title: "",
+      items: [
+        { id: uuidv4(), content: "" },
+        { id: uuidv4(), content: "" },
+        { id: uuidv4(), content: "" },
+        { id: uuidv4(), content: "" },
+        { id: uuidv4(), content: "" },
+      ],
+    });
   };
 
   const [draggedItem, setDraggedItem] = useState(null);
@@ -33,47 +69,52 @@ const TopFiveList = () => {
   };
 
   const onDragOver = (index) => {
-    const draggedOverItem = items[index];
+    const draggedOverItem = list.items[index];
 
     if (draggedOverItem === draggedItem) {
       return;
     }
 
-    let itemsCopy = [...items];
+    let itemsCopy = [...list.items];
 
     itemsCopy.splice(index, 0, itemsCopy.splice(draggedItem, 1)[0]);
 
-    setItems(itemsCopy);
+    setList((prevState) => {
+      return { ...prevState, items: itemsCopy };
+    });
     setDraggedItem(index);
   };
 
-  const itemElements = items.map((item, index) => (
-    <div
-      key={item.id}
-      className="item"
-      onDragStart={(e) => onDragStart(e, index)}
-      onDragOver={() => onDragOver(index)}
-      draggable
-    >
+  return (
+    <>
       <div>
-        Item {index + 1}:
+        <label htmlFor="title">List Title:</label>
         <input
           type="text"
-          value={item.content}
-          onChange={(e) => handleChange(e, item.id)}
+          id="title"
+          value={list.title}
+          onChange={handleTitleChange}
         />
       </div>
-    </div>
-  ));
-
-  return (
-    <form className="container" onSubmit={handleSubmit}>
-      <h2>Top Five List</h2>
-
-      {itemElements}
-
-      <button type="submit">Save</button>
-    </form>
+      <ol className="container">
+        {list.items.map((item, index) => (
+          <li
+            key={item.id}
+            className="item"
+            onDragStart={(e) => onDragStart(e, index)}
+            onDragOver={() => onDragOver(index)}
+            draggable
+          >
+            <input
+              type="text"
+              value={item.content}
+              onChange={(e) => handleChange(e, item.id)}
+            />
+          </li>
+        ))}
+        <button onClick={handleSave}>Save</button>
+      </ol>
+    </>
   );
 };
 
